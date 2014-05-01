41c09d9607b183a7c2a35a6200d239927c4fc75a
Tong Su <quill23@126.com>
2014-05-01 16:02:34 +0800

    Improving data generating algorithms, make it using less memory and generating data faster when running.

--- data/data_generator.py
+++ data/data_generator.py
@@ -12,33 +12,29 @@ class DataGenerator:
     Test Data Generator.
     '''
 
-    _temp_path = None
     _temp_dict = None
-    _data_box = None
     _output_path = None
+    _ignore_key_list = None
 
-    _x_init = 1
-    _x_loop = 1000
+    _x_loop = 10
     _y_loop = 1
-    _z_loop = 1
     _x_step = 1
     _y_step = 1
-    _z_step = 1
 
     def __init__(self, temp_path, output_path, config_dict):
         '''
         Init template information
         '''
 
-        self._temp_path = temp_path
-
-        temp_file = open(self._temp_path, 'r')
+        temp_file = open(temp_path, 'r')
         self._temp_dict = json.load(temp_file)
+        temp_file.close()
 
         self._output_path = output_path
+        self._ignore_key_list = ['type']
+
 
         if len(config_dict.keys()) != 0:
-            self._x_init = config_dict['x_init']
             self._x_loop = config_dict['x_loop']
             self._y_loop = config_dict['y_loop']
             self._x_step = config_dict['x_step']
@@ -47,86 +43,95 @@ class DataGenerator:
         if self._x_step > self._x_loop \
             or self._y_step > self._x_loop:
             print 'ERROR! STEP IS LARGER THAN LOOP NUM.'
-            quit()
-
-        self._data_box = [[]]
-        # _data_box will store all the generated data
-        # and the data in box will output to the data file.
-        # Structure: [x[y]]
+            exit(1)
 
 
-    def x_gen(self):
+    def _x_ext(self, dic):
         '''
-        Expend the length of single value in template.
-        x means single value length.
+        Extend the x axis.
         '''
 
-        def str_gen(num):
-            for i in xrange(num):
-                yield 'g'
+        for key in dic:
+            if key in self._ignore_key_list:
+                continue
 
-        def ext_dict(dic, length):
-            for key in dic.keys():
-                if type(dic[key]) is unicode:
-                    dic[key] = ''.join(str_gen(self._x_init + length)) \
-                            if dic[key] == '' else dic[key]
-                elif type(dic[key]) is list:
-                    for item in dic[key]:
-                        ext_dict(item, length)
-                elif type(dic[key]) is dict:
-                    ext_dict(dic[key], length)
+            value_type = type(dic[key])
+                
+            if value_type is unicode:
+                dic[key] += 'g'
+            elif value_type is list:
+                for item in dic[key]:
+                    self._x_ext(item)
+            elif value_type is dict:
+                self._x_ext(dic[key])
+            else:
+                pass
+
+
+    def _y_ext(self, dic):
+        '''
+        Extend the y axis.
+        '''
+
+        for key in dic:
+            if key in self._ignore_key_list:
+                continue
+
+            value_type = type(dic[key])
+
+            if value_type is list:
+                item = None
+                for item in dic[key]:
+                    self._y_ext(item)
+                if item is None:
+                    dic[key].append(dict())
                 else:
-                    pass
+                    dic[key].append(item)
+            elif value_type is dict:
+                self._x_ext(dic[key])
+            else:
+                pass
 
-        for length in xrange(1, self._x_loop + 1, self._x_step):
-            obj_dict = copy.deepcopy(self._temp_dict)
-            ext_dict(obj_dict, length)
-            self._data_box[0].append(obj_dict)
 
-
-    def y_gen(self):
+    def fast_gen(self):
         '''
-        Expend the repeat value in template.
-        y means repeat value.
+        New generator algorithms, save huge amount of memory.
         '''
 
-        def ext_list(dic, repeat):
-            for key in dic.keys():
-                if type(dic[key]) is list:
-                    ext_list(dic[key][0], repeat)
-                    for i in range(repeat):
-                        dic[key].append(copy.copy(dic[key][0]))
-                elif type(dic[key]) is dict:
-                    ext_list(dic[key], repeat)
-                else:
-                    pass
+        x_dict = copy.deepcopy(self._temp_dict)
 
-        for repeat in xrange(1, self._y_loop, self._y_step):
-            obj_list = copy.deepcopy(self._data_box[0])
-            for item in obj_list:
-                ext_list(item, repeat)
-            self._data_box.append(obj_list)
+        for x in xrange(0, self._x_loop, self._x_step):
+            # Extend the x axis
+            self._x_ext(x_dict)
+            y_dict = copy.deepcopy(x_dict)
+            
+            for y in xrange(0, self._y_loop, self._y_step):
+                # Extend the y axis
+                self._y_ext(y_dict)
+                
+                file_name = '%d.%d.json' % (x, y)
+                data_file = open(self._output_path + '/' + file_name, 'w')
+                json.dump(y_dict, data_file)
+                data_file.close()
+
+            del y_dict
+        del x_dict
 
 
     def gen(self):
         '''
+        The gen method has been changed in new git branch, it change the 
+        data generating algorithms to avoid overhead memory requirement
+        in large data set situation.
+
+        The gen method is no more recommanded, its implementation has also
+        been replaced by the new fast_gen method.
+
+        The obsolete description as below:
         Call the *_gen methods and generate data box.
         And output data box to file.
         '''
-        
-        print '    Generating single value'
-        self.x_gen()
-
-        print '    Generating repeat value'
-        self.y_gen()
-
-        print '    Output to file'
-        for y in xrange(self._y_loop):
-            for x in xrange(self._x_loop):
-                file_name = '.'.join([str(x), str(y)]) + '.json'
-                data_file = open(self._output_path + file_name, 'w')
-                json.dump(self._data_box[y][x], data_file)
-                data_file.close()
+        self.fast_gen()
 
 
 def del_org(dir, postfix):
